0179b664eda9dc2956ecf86fa4b5e6b84ea755a1
idoasisay <59815596+idoasisay@users.noreply.github.com>
2020-03-06 20:11:56 +0900

    라우터 연결 및 퍼그 작업 중

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-// for core-js v3:
+//express를 선언해 주고
 import express from "express";
+
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+// for core-js v3:
 import "core-js";
 //default로 export를 하지 않으면 괄호를 써야 함. 그녀석만..
 import userRouter from "./routers/userRouter";
@@ -11,12 +13,20 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
+//express 실행한 걸 변수 app에 담았음
 const app = express();
 
+//pug 세팅. pug는 express의 템플릿이다.
+app.set('view engine', "pug");
+//미들웨어를 추가함
+//쿠키를 전달받아서 사용할수 있도록 만들어줌
 app.use(cookieParser());
+//사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어. body를 검사해서 파싱해 줌
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+//정보 보안
 app.use(helmet());
+//모든 일들을 로깅함
 app.use(morgan("dev"));
 
 //누군가 user에 들어왔다면, 이 라우터 전체를 사용하겠다는 의미.
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,8 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+
+export const users = (req, res) => res.render("users");
+export const usersDetail = (req, res) => res.render("usersDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const changePassword = (req, res) => res.render("changePassword");
